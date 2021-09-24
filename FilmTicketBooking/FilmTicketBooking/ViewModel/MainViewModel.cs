c57e1c1b3acf0746e0719dbfdd0ff27688931428
Thanh Dat <ddatdt12@gmail.com>
2021-09-24 11:18:10 +0700

    Create skeleton project

---
+++ FilmTicketBooking/FilmTicketBooking/ViewModel/MainViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace FilmTicketBooking.ViewModel
+{
+    public class MainViewModel : BaseViewModel
+    {
+        public MainViewModel()
+        {
+            MessageBox.Show("Da vao trong MainViewModel");
+        }
+    }
+}
