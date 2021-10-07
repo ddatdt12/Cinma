55b09b9998456c322a2f2bb326cede84258bc9e1
TranDKhoi <tranxyz88@gmail.com>
2021-10-07 11:54:26 +0700

    thêm cửa sổ main admin và chỉnh sửa binding+command

---
+++ FilmTicketBooking/FilmTicketBooking/ViewModel/AdminVM/MainAdminViewModel.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace FilmTicketBooking.ViewModel
+{
+    
+    public class MainAdminViewModel : BaseViewModel
+    {
+        public ICommand SignoutCM { get; set; }
+
+
+        public MainAdminViewModel()
+        {
+            SignoutCM = new RelayCommand<FrameworkElement>((p) =>
+            {
+                return p == null ? false : true;
+            }, (p) =>
+            {
+                FrameworkElement window = GetParentWindow(p);
+                var w = window as Window;
+               if (w!=null)
+                {
+                    w.Hide();
+                    LoginWindow w1 = new LoginWindow();
+                    w1.ShowDialog();
+                    w.Close();
+                }
+            });
+        }
+
+        FrameworkElement GetParentWindow(FrameworkElement p)
+        {
+            FrameworkElement parent = p;
+
+            while (parent.Parent != null)
+            {
+                parent = parent.Parent as FrameworkElement;
+            }
+            return parent;
+        }
+
+    }
+}
