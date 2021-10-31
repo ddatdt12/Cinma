a6aeb2bb5942fc2ed9cea0b6fe27ca41a925569d
TranDKhoi <tranxyz88@gmail.com>
2021-10-31 15:28:58 +0700

    QLSC
    Đổi validate sang messbox
    Xoá country source
    Lọc theo phòng
    scrollviewer inside listview
    cửa sổ add suất chiếu (chưa add được)

--- CinemaManagement/CinemaManagement/ViewModel/AdminVM/ShowtimeManagementVM/Add.ShowtimeWindowViewModel.cs
+++ CinemaManagement/CinemaManagement/ViewModel/AdminVM/ShowtimeManagementVM/Add.ShowtimeWindowViewModel.cs
@@ -17,7 +17,34 @@ namespace CinemaManagement.ViewModel.AdminVM.ShowtimeManagementViewModel
 
         public void SaveShowtimeFunc(Window p)
         {
+            if (IsValidData())
+            {
+                ShowtimeDTO temp = new ShowtimeDTO
+                {
+                    MovieId = movieSelected.Id,
+                    Movie = movieSelected,
+                    RoomId = ShowtimeRoom.Id,
+                    ShowDate = Showtime,
+                    StartTime = Showtime.TimeOfDay,
+                };
 
+
+                (bool IsSuccess, string message) = ShowtimeService.Ins.AddShowtime(temp);
+
+
+                if (IsSuccess)
+                {
+                    MessageBox.Show(message);
+                    ReloadShowtimeList();
+                    p.Close();
+                }
+                else
+                {
+                    MessageBox.Show(message);
+                }
+            }
+            else
+                MessageBox.Show("Vui lòng nhập đầy đủ thông tin!");
         }
     }
 }
