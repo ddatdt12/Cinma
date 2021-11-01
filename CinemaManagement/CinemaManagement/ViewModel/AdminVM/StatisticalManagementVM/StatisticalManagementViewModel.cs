ddff14d970d524e40b24b75c17a1ac6ef8f48198
TranDKhoi <tranxyz88@gmail.com>
2021-11-01 11:13:19 +0700

    thống kê
    tạo chart chưa đổ dữ liệu

---
+++ CinemaManagement/CinemaManagement/ViewModel/AdminVM/StatisticalManagementVM/StatisticalManagementViewModel.cs
@@ -0,0 +1,44 @@
+﻿using LiveCharts;
+using LiveCharts.Wpf;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CinemaManagement.ViewModel.AdminVM.StatisticalManagementVM
+{
+   public partial class StatisticalManagementViewModel:BaseViewModel,INotifyPropertyChanged
+    {
+        public SeriesCollection SeriesCollection { get; set; }
+        public string[] Labels { get; set; }
+        public Func<double,string> Formatter { get; set; }
+
+
+        public StatisticalManagementViewModel()
+        {
+            List<double> temp = new List<double>();
+            for (int i =0;i<30;i++)
+            {
+                temp.Add(i);
+            }
+
+            SeriesCollection = new SeriesCollection
+            {
+                new ColumnSeries
+                {
+                    Title = "Vé",
+                    Values = new ChartValues<double>(temp),
+                },
+                new ColumnSeries
+                {
+                    Title = "Đồ ăn",
+                    Values = new ChartValues<double>(temp),
+                }
+            };
+        }
+
+
+    }
+}
