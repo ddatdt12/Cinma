23d9bbb1baa28826d67c5416b0f141c04afeff16
thaoht194 <thaohtgm@gmail.com>
2021-11-17 23:59:34 +0700

    Hoàn thành trang đặt và fix xong các lỗi

--- CinemaManagement/CinemaManagement/DTOs/ProductDTO.cs
+++ CinemaManagement/CinemaManagement/DTOs/ProductDTO.cs
@@ -1,5 +1,6 @@
 ﻿using CinemaManagement.Utils;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.IO;
 using System.Windows.Media;
 
@@ -10,34 +11,60 @@ namespace CinemaManagement.DTOs
         public ProductDTO()
         {
         }
+
+        public ProductDTO(int id = 0,
+                          string displayname = "Không xác định",
+                          string category = "Không xác định",
+                          decimal price = 0,
+                          string imagesource = "",
+                          int quantity = 1)
+        {
+            this.Id = id;
+            this.DisplayName = displayname;
+            this.Category = category;
+            this.Price = price;
+            this.Quantity = quantity;
+            this.Image = imagesource;
+        }
+        public ProductDTO(ProductDTO product)
+        {
+            this.Id = product.Id;
+            this.DisplayName = product.DisplayName;
+            this.Category = product.Category;
+            this.Price = product.Price;
+            this.Quantity = product.Quantity;
+            this.Image = product.Image;
+            this.ImgSource = product.ImgSource;
+        }
         public int Id { get; set; }
         public string DisplayName { get; set; }
         public string Category { get; set; }
         public decimal Price { get; set; }
         public int Quantity { get; set; }
+        public string Image { get; set; }
 
-        private string _image;
-        public string Image
+        public ImageSource _imgSource;
+        public ImageSource ImgSource
         {
             get
             {
-                return _image;
+                if (_imgSource is null)
+                {
+                    if (File.Exists(Helper.GetMovieImgPath(Image)))
+                    {
+                        _imgSource = Helper.GetImageSource(Image);
+                    }
+                    else
+                    {
+                        _imgSource = Helper.GetImageSource("null.jpg");
+                    }
+                }
+                return _imgSource;
             }
             set
             {
-                _image = value;
-                if (File.Exists(Helper.GetProductImgPath()))
-                {
-                    ImgSource = Helper.GetImageSource(_image);
-                }
-                else
-                {
-                    _image = "null.jpg";
-                    ImgSource = Helper.GetImageSource("null.jpg");
-                }
+                _imgSource = value;
             }
         }
-        public ImageSource ImgSource { get; set; }
-
     }
 }
