e94a835247443523ed3e2ebcac5d40140a872222
TranDKhoi <tranxyz88@gmail.com>
2021-11-19 00:35:55 +0700

    phân chia chức năng thống kê

--- CinemaManagement/CinemaManagement/ViewModel/AdminVM/StatisticalManagementVM/StatisticalManagementViewModel.cs
+++ CinemaManagement/CinemaManagement/ViewModel/AdminVM/StatisticalManagementVM/StatisticalManagementViewModel.cs
@@ -1,24 +1,26 @@
-﻿using LiveCharts;
+﻿using CinemaManagement.Views.Admin.StatisticalManagement;
+using LiveCharts;
 using LiveCharts.Wpf;
+using MaterialDesignThemes.Wpf;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
+using System.Windows.Media;
 
 namespace CinemaManagement.ViewModel.AdminVM.StatisticalManagementVM
 {
     public partial class StatisticalManagementViewModel : BaseViewModel, INotifyPropertyChanged
     {
-
+        public Frame mainFrame { get; set; }
+        public Card ButtonView { get; set; }
         private int _SelectedPeriod;
         public int SelectedPeriod
         {
             get { return _SelectedPeriod; }
             set { _SelectedPeriod = value; OnPropertyChanged(); }
         }
-
-
 
 
         public SeriesCollection SeriesCollection { get; set; }
@@ -30,11 +32,11 @@ namespace CinemaManagement.ViewModel.AdminVM.StatisticalManagementVM
 
         public ChartValues<double> Food { get; set; }
         public ChartValues<double> Ticket { get; set; }
-        public List<string> YearSource { get; set; }
 
-
-        public ICommand SelectedPeriodCM { get; set; }
-
+        public ICommand LoadViewCM { get; set; }
+        public ICommand StoreButtonNameCM { get; set; }
+        public ICommand LoadAllStatisticalCM { get; set; }
+        public ICommand LoadRankStatisticalCM { get; set; }
 
 
 
@@ -44,9 +46,6 @@ namespace CinemaManagement.ViewModel.AdminVM.StatisticalManagementVM
 
         public StatisticalManagementViewModel()
         {
-            YearSource = new List<string>();
-            YearSource = GetYearCombobox();
-
 
             List<double> temp = new List<double>();
             List<double> temp1 = new List<double>();
@@ -105,55 +104,38 @@ namespace CinemaManagement.ViewModel.AdminVM.StatisticalManagementVM
                 }
             };
 
-
-            SelectedPeriodCM = new RelayCommand<ComboBox>((p) => { return true; }, (p) =>
+            LoadViewCM = new RelayCommand<Frame>((p) => { return true; }, (p) =>
             {
-                if (SelectedPeriod == 0)
-                {
-
-                    p.ItemsSource = null;
-                    p.Items.Clear();
-
-                    p.ItemsSource = GetYearCombobox();
-                }
-                else
-                {
-                    p.ItemsSource = null;
-                    p.Items.Clear();
-
-                    List<string> monthsource = new List<string>();
-                    monthsource.Add("Tháng 1");
-                    monthsource.Add("Tháng 2");
-                    monthsource.Add("Tháng 3");
-                    monthsource.Add("Tháng 4");
-                    monthsource.Add("Tháng 5");
-                    monthsource.Add("Tháng 6");
-                    monthsource.Add("Tháng 7");
-                    monthsource.Add("Tháng 8");
-                    monthsource.Add("Tháng 9");
-                    monthsource.Add("Tháng 10");
-                    monthsource.Add("Tháng 11");
-                    monthsource.Add("Tháng 12");
-
-                    p.ItemsSource = monthsource;
-                }
+                mainFrame = p;
+                p.Content = new IncomeStatistical();
             });
+            StoreButtonNameCM = new RelayCommand<Card>((p) => { return true; }, (p) =>
+            {
+                ButtonView = p;
+                p.Background = (SolidColorBrush)new BrushConverter().ConvertFromString("#fafafa");
+                p.SetValue(ShadowAssist.ShadowDepthProperty, ShadowDepth.Depth0);
 
+            });
+            LoadAllStatisticalCM = new RelayCommand<Card>((p) => { return true; }, (p) =>
+            {
+                ChangeView(p);
+                mainFrame.Content = new IncomeStatistical();
+
+            });
+            LoadRankStatisticalCM = new RelayCommand<Card>((p) => { return true; }, (p) =>
+            {
+                ChangeView(p);
+                mainFrame.Content = new RankingStatistical();
+            });
         }
 
-
-        public void ChangeComboboxItem()
+        public void ChangeView(Card p)
         {
-            MessageBox.Show("alo");
+            ButtonView.Background = (SolidColorBrush)new BrushConverter().ConvertFromString("#f0f2f5");
+            ButtonView.SetValue(ShadowAssist.ShadowDepthProperty, ShadowDepth.Depth2);
+            ButtonView = p;
+            p.Background = (SolidColorBrush)new BrushConverter().ConvertFromString("#fafafa");
+            p.SetValue(ShadowAssist.ShadowDepthProperty, ShadowDepth.Depth0);
         }
-        public List<string> GetYearCombobox()
-        {
-            List<string> yearsource = new List<string>();
-            yearsource.Add("2019");
-            yearsource.Add("2020");
-            yearsource.Add("2021");
-            return yearsource;
-        }
-
     }
 }
---
+++ CinemaManagement/CinemaManagement/Views/Admin/StatisticalManagement/IncomeStatistical.xaml.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CinemaManagement.Views.Admin.StatisticalManagement
+{
+    /// <summary>
+    /// Interaction logic for IncomeStatistical.xaml
+    /// </summary>
+    public partial class IncomeStatistical : Page
+    {
+        public IncomeStatistical()
+        {
+            InitializeComponent();
+        }
+
+        private void periodbox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            ComboBoxItem s = (ComboBoxItem)periodbox.SelectedItem;
+            switch (s.Content.ToString())
+            {
+                case "Theo năm":
+                    {
+                        GetYearSource(Timebox);
+                        return;
+                    }
+                case "Theo tháng":
+                    {
+                        GetMonthSource(Timebox);
+                        return;
+                    }
+            }
+        }
+        private void periodbox_Loaded(object sender, System.Windows.RoutedEventArgs e)
+        {
+            GetYearSource(Timebox);
+        }
+        public void GetYearSource(ComboBox cbb)
+        {
+            if (cbb is null) return;
+
+            List<string> l = new List<string>();
+
+            l.Add("2019");
+            l.Add("2020");
+            l.Add("2021");
+
+            cbb.ItemsSource = l;
+            cbb.SelectedIndex = 2;
+        }
+        public void GetMonthSource(ComboBox cbb)
+        {
+            if (cbb is null) return;
+
+            List<string> l = new List<string>();
+
+            l.Add("Tháng 1");
+            l.Add("Tháng 2");
+            l.Add("Tháng 3");
+            l.Add("Tháng 4");
+            l.Add("Tháng 5");
+            l.Add("Tháng 6");
+            l.Add("Tháng 7");
+            l.Add("Tháng 8");
+            l.Add("Tháng 9");
+            l.Add("Tháng 10");
+            l.Add("Tháng 11");
+            l.Add("Tháng 12");
+
+            cbb.ItemsSource = l;
+            cbb.SelectedIndex = 0;
+        }
+    }
+}
