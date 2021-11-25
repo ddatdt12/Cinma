c4b92632deab92bf08535176e88c5c5dd59ec8da
TranDKhoi <tranxyz88@gmail.com>
2021-11-25 22:09:54 +0700

    đoạn code gửi mail test

--- CinemaManagement/CinemaManagement/ViewModel/LoginVM/ForgotPassViewModel.cs
+++ CinemaManagement/CinemaManagement/ViewModel/LoginVM/ForgotPassViewModel.cs
@@ -1,4 +1,7 @@
-﻿using System.Windows;
+﻿using System;
+using System.Net;
+using System.Net.Mail;
+using System.Windows;
 using System.Windows.Input;
 
 namespace CinemaManagement.ViewModel
@@ -21,6 +24,37 @@ namespace CinemaManagement.ViewModel
                 {
                     w.Close();
                 }
+            });
+
+            ConfirmCM = new RelayCommand<object>((p) => { return true; }, (p) =>
+            {
+
+                //string to, from, pass, messageBody;
+                //MailMessage message = new MailMessage();
+                //to = "20520594@gm.uit.edu.vn";
+                //from = "20520594@gm.uit.edu.vn";
+                //pass = "passhere";
+                //messageBody = "this is body";
+                //message.To.Add(to);
+                //message.From = new MailAddress(from);
+                //message.Body = "From : " + "<br>Message: " + messageBody;
+                //message.Subject = "this is subject";
+                //message.IsBodyHtml = true;
+                //SmtpClient smtp = new SmtpClient("smtp.gmail.com");
+                //smtp.EnableSsl = true;
+                //smtp.Port = 587;
+                //smtp.DeliveryMethod = SmtpDeliveryMethod.Network;
+                //smtp.Credentials = new NetworkCredential(from, pass);
+
+                //try
+                //{
+                //    smtp.Send(message);
+                //}
+                //catch (Exception ex)
+                //{
+                //    MessageBox.Show(ex.Message);
+                //}
+
             });
         }
 
