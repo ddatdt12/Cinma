f4c67f1e4d1e820280bec2e671dc6d6998409c9e
phongpro279 <75621558+phongpro279@users.noreply.github.com>
2021-11-28 11:58:15 +0700

    Custom Message Box

--- CinemaManagement/CinemaManagement/DTOs/ProductDTO.cs
+++ CinemaManagement/CinemaManagement/DTOs/ProductDTO.cs
@@ -22,20 +22,6 @@ namespace CinemaManagement.DTOs
             this.Image = image;
             this.Quantity = quantity;
         }
-        public ProductDTO(int id,
-                          string displayname,
-                          string category,
-                          decimal price,
-                          string image,
-                          int quantity)
-        {
-            this.Id = id;
-            this.DisplayName = displayname;
-            this.Category = category;
-            this.Price = price;
-            this.Image = image;
-            this.Quantity = quantity;
-        }
         public int Id { get; set; }
         public string DisplayName { get; set; }
         public string Category { get; set; }
