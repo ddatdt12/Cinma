ccd1b4be733a0916c90c419fb4bb08088edc1fd8
thaoht194 <thaohtgm@gmail.com>
2021-11-29 13:05:13 +0700

    Fix lỗi merge

---
+++ CinemaManagement/CinemaManagement/ViewModel/StaffViewModel/DeviceProblemsWindowVM/AddDeviceProblemPageViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CinemaManagement.ViewModel.StaffViewModel.DeviceProblemsWindowVM
+{
+    public class AddDeviceProblemPageViewModel: BaseViewModel
+    {
+
+        public AddDeviceProblemPageViewModel()
+        {
+
+        }
+    }
+}
