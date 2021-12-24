9c8df8de2b010495ba3daa6a83f8ee670c41c710
TranDKhoi <tranxyz88@gmail.com>
2021-12-25 01:30:00 +0700

    thêm chuyển role admin

---
+++ CinemaManagement/CinemaManagement/ViewModel/StaffViewModel/IsAdminConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace CinemaManagement.ViewModel.StaffViewModel
+{
+
+    public class IsAdminConverter : IValueConverter
+    {
+        // This converts the result object to the foreground.
+        public object Convert(object value, Type targetType,
+            object parameter, CultureInfo language)
+        {
+            // Retrieve the format string and use it to format the value.
+
+            if ((bool)value == true)
+                return Visibility.Visible;
+            else
+                return Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
